4c461d033b8546472d94b1224aeb41a595764628
Neville Guimarães <neville.guimaraes@poncetech.com.br>
2024-01-29 16:27:10 -0300

    feat: adicionando test de atualizacao e exclusao de post

--- tests/post/conftest.py
+++ tests/post/conftest.py
@@ -4,6 +4,25 @@ from models import User, Post
 from factory import db
 
 
+@pytest.fixture()
+def seed_post():
+    posts = [
+        {
+            'text': 'texto do post 1',
+            'author_id': 1
+        },
+        {
+            'text': 'texto do post 2',
+            'author_id': 2
+        }
+    ]
+
+    posts_db = [Post(**post) for post in posts]
+
+    db.session.add_all(posts_db)
+    db.session.commit()
+
+
 @pytest.fixture(scope='function', autouse=True)
 def clean_db(request):
     def teardown():
--- tests/post/test_post.py
+++ tests/post/test_post.py
@@ -7,3 +7,32 @@ def test_create_post(test_client, seed_db, access_token):
 
     assert response.status_code == 201
     assert response.json['msg'] == 'Post criado com sucesso.'
+
+
+def test_update_post(test_client, seed_db, access_token, seed_post):
+    obj = {
+        'text': 'Teste de atualizacao de post'
+    }
+
+    response = test_client.put('/posts/1', json=obj, headers=access_token)
+
+    assert response.status_code == 200
+    assert response.json['msg'] == 'Post atualizado com sucesso.'
+
+
+def test_denied_update_post(test_client, seed_db, access_token, seed_post):
+    obj = {
+        'text': 'Teste de atualização negada de post'
+    }
+
+    response = test_client.put('/posts/2', json=obj, headers=access_token)
+
+    assert response.status_code == 403
+    assert response.json['msg'] == 'Você não tem autorização para atualizar este post.'
+
+
+def test_delete_post(test_client, seed_db, access_token, seed_post):
+    response = test_client.delete('/posts/1', headers=access_token)
+
+    assert response.status_code == 200
+    assert response.json['msg'] == 'Post deletado com sucesso.'
